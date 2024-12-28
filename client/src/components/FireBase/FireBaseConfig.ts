b87c7da5b14dfb29167a1b612a697e32cfdc3caa
adarsh <111566026+adarshpatel111@users.noreply.github.com>
2024-12-28 15:46:10 +0530

    Update FireBaseConfig.ts

--- client/src/components/FireBase/FireBaseConfig.ts
+++ client/src/components/FireBase/FireBaseConfig.ts
@@ -1,24 +1,23 @@
-// src/FireBaseConfig.ts
-
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";  // Realtime Database import
 
-// Use Vite environment variables
+// Firebase config
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: "https://collab-bros-back-default-rtdb.firebaseio.com/", // Keep your database URL static if not provided by environment variables
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
+const analytics = getAnalytics(app);  
+const db = getDatabase(app);  
 
-export { db, analytics };
+export { db, analytics }; 
