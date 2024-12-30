cf41f24074fb0d39000ef6cddc1dc8e968674fed
adarsh <111566026+adarshpatel111@users.noreply.github.com>
2024-12-30 13:10:01 +0530

    Create rootColors.tsx

---
+++ client/src/Utilities/rootColors.tsx
@@ -0,0 +1,10 @@
+import { rootColorsType } from "./types";
+
+export const rootColors: rootColorsType = {
+  primary: "#0F2E44",
+  secondary: "#EC681E",
+  yellow: "#FFDD00",
+  white: "#ffffff",
+  green: "#87BB47",
+  grey: "#8D8C8D",
+};
