9bbf9d8bc639019022990b796c289a63ac95a604
adarsh <111566026+adarshpatel111@users.noreply.github.com>
2024-12-30 13:10:04 +0530

    Create types.tsx

---
+++ client/src/Utilities/types.tsx
@@ -0,0 +1,43 @@
+export type navLinksType = {
+  title: string;
+  path: string;
+};
+
+export type rootColorsType = {
+  primary: string;
+  secondary: string;
+  yellow: string;
+  white: string;
+  green: string;
+  grey: string;
+};
+
+export type CardPropsType = {
+  lottie: object;
+  text: string;
+};
+
+export type WeOfferCardDataTypes = {
+  title: string;
+  desc: string;
+  img: string;
+};
+export type ServicesSectionDetailsType = {
+  header: string;
+  title: string;
+  desc: string;
+  mainLottie: object;
+};
+export type ServicesSectionDataType = {
+  title: string;
+  desc: string;
+  icon: JSX.Element;
+};
+
+export type exploreCardDataType = {
+  img: string;
+  summary: string;
+  title: string;
+  desc: string;
+  icon: JSX.Element;
+};
